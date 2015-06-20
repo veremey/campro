b6603295b2b206713ddfd3a91c7cdb9ed265cd85
Veremey <Veremey@i.ua>
2015-06-21 01:50:52 +0300

    cmp

--- js/common.js
+++ js/common.js
@@ -235,6 +235,42 @@ $('.js-select').click(function(event){
     event.stopPropagation();
 });
 
+// choose
+
+
+    function choose() {
+        var number = $(".js-choose");
+        number.each(function(){
+            var max_number = +($(this).attr("data-max-number"));
+            var input = $(this).find("input");
+            var plus = $(this).find(".js-plus");
+            var minus = $(this).find(".js-minus");
+            plus.bind("click", function(){
+                var val = +(input.val());
+                if (val >= max_number) {
+                    return false
+                }
+                else {
+                    val += 1;
+                    input.val(val);
+                }
+            });
+            minus.bind("click", function(){
+                var val = +(input.val());
+                if (val > 1) {
+                    val -= 1;
+                    input.val(val);
+                }
+                else {
+                    return false;
+                }
+            });
+        });
+    }
+    choose();
+
+
+// slick carousel
 
 	$('.js-carousel').slick({
 		slidesToShow: 1,
