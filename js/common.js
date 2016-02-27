65fc772015fe6cdafc5a2e77de4a0515111672f4
Veremey <veremey4uk@gmail.com>
2016-02-27 16:56:35 +0200

    presentation

--- js/common.js
+++ js/common.js
@@ -64,10 +64,7 @@ $(document).ready(function() {
 
 });
 
-
-
 //  FORM VALIDATION
-
 
 $(document).ready(function() {
 		function validate() {
@@ -155,36 +152,36 @@ $(document).ready(function() {
 // polzunok
 
 function ui_slider() {
-		$(".js-ui-slider").each(function(){
-				var slider = $(this).find(".js-ui-slider-main");
-				var input_from = $(this).find(".js-ui-slider-from");
-				var input_to = $(this).find(".js-ui-slider-to");
-				slider.slider({
-						range: true,
-						min: 0,
-						max: 23500,
-						step: 5,
-						values: [ 12595, 23500 ],
-						slide: function( event, ui ) {
-								$(this).find(".ui-slider-handle").html("<span></span>");
-								var handle_0 = $(this).find(".ui-slider-range").next().find("span")
-								var handle_1 = $(this).find(".ui-slider-range").next().next().find("span");
-								input_from.val(ui.values[0]);
-								input_to.val(ui.values[1]);
-								handle_0.text(ui.values[0]);
-								handle_1.text(ui.values[1]);
-						}
-				});
-				console.log(handle_0);
-				console.log(handle_1);
+	$(".js-ui-slider").each(function(){
+		var slider = $(this).find(".js-ui-slider-main");
+		var input_from = $(this).find(".js-ui-slider-from");
+		var input_to = $(this).find(".js-ui-slider-to");
+		slider.slider({
+			range: true,
+			min: 0,
+			max: 23500,
+			step: 5,
+			values: [ 12595, 23500 ],
+			slide: function( event, ui ) {
 				$(this).find(".ui-slider-handle").html("<span></span>");
 				var handle_0 = $(this).find(".ui-slider-range").next().find("span")
 				var handle_1 = $(this).find(".ui-slider-range").next().next().find("span");
-				handle_0.text(slider.slider( "values", 0 ));
-				handle_1.text(slider.slider( "values", 1 ));
-				input_from.val(slider.slider( "values", 0 ));
-				input_to.val(slider.slider( "values", 1 ));
+				input_from.val(ui.values[0]);
+				input_to.val(ui.values[1]);
+				handle_0.text(ui.values[0]);
+				handle_1.text(ui.values[1]);
+			}
 		});
+		console.log(handle_0);
+		console.log(handle_1);
+		$(this).find(".ui-slider-handle").html("<span></span>");
+		var handle_0 = $(this).find(".ui-slider-range").next().find("span")
+		var handle_1 = $(this).find(".ui-slider-range").next().next().find("span");
+		handle_0.text(slider.slider( "values", 0 ));
+		handle_1.text(slider.slider( "values", 1 ));
+		input_from.val(slider.slider( "values", 0 ));
+		input_to.val(slider.slider( "values", 1 ));
+	});
 }
 ui_slider();
 
@@ -237,37 +234,36 @@ $('.js-select').click(function(event){
 
 // choose
 
-
-    function choose() {
-        var number = $(".js-choose");
-        number.each(function(){
-            var max_number = +($(this).attr("data-max-number"));
-            var input = $(this).find("input");
-            var plus = $(this).find(".js-plus");
-            var minus = $(this).find(".js-minus");
-            plus.bind("click", function(){
-                var val = +(input.val());
-                if (val >= max_number) {
-                    return false
-                }
-                else {
-                    val += 1;
-                    input.val(val);
-                }
-            });
-            minus.bind("click", function(){
-                var val = +(input.val());
-                if (val > 1) {
-                    val -= 1;
-                    input.val(val);
-                }
-                else {
-                    return false;
-                }
-            });
-        });
-    }
-    choose();
+function choose() {
+  var number = $(".js-choose");
+  number.each(function(){
+    var max_number = +($(this).attr("data-max-number"));
+    var input = $(this).find("input");
+    var plus = $(this).find(".js-plus");
+    var minus = $(this).find(".js-minus");
+    plus.bind("click", function(){
+      var val = +(input.val());
+      if (val >= max_number) {
+          return false
+      }
+      else {
+          val += 1;
+          input.val(val);
+      }
+    });
+    minus.bind("click", function(){
+      var val = +(input.val());
+      if (val > 1) {
+        val -= 1;
+        input.val(val);
+      }
+      else {
+        return false;
+      }
+    });
+  });
+}
+choose();
 
 
 // slick carousel
